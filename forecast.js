8b130a1859557fe4c4a9a707d3f818b5140762fc
Alison Graham <42179683+AlisonGraham75@users.noreply.github.com>
2020-06-16 14:57:50 -0600

    Removed expired access key

--- forecast.js
+++ forecast.js
@@ -2,7 +2,7 @@ const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
     
-    const url = 'http://api.weatherstack.com/current?access_key=c44bf181e3f9b36c3997ffc12b71f126&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=<ACCESS_KEY>&query=' + latitude + ',' + longitude + '&units=f'
     request({ url, json: true }, (error, { body }) => {
 
              if (error) {
